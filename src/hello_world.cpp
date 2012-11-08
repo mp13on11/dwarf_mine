b344b17d45762ed499cc05edf886cb0bf84d746b
mp13on11 <>
2012-11-08 20:46:14 +0100

    C++ 11 awesomeness

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char**argv) {
-    cout << "Hello World!" << endl;
+int main(int argc, char** argv) {
+    vector<string> arguments(argv + 1, argv + argc);
+    
+    if (arguments.empty())
+        arguments.push_back("World");
+    
+    for (const string& arg : arguments)
+        cout << "Hello " << arg << "!" << endl;
 }
