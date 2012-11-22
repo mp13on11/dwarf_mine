ec95e94fc35e8307ee68d1e019f1f959cf8c1c1d
mp13on11 <>
2012-11-22 18:29:09 +0100

    make Benchmark pass command line args

--- src/lib/benchmark/main.cpp
+++ src/lib/benchmark/main.cpp
@@ -1,13 +1,26 @@
 #include "benchmark/BenchmarkKernel.h"
 
+#include <iostream>
+
+using namespace std;
+
 BenchmarkKernel::~BenchmarkKernel()
 {
 }
 
-int main(int argc, const char *argv[])
+int main(int argc, const char* argv[])
 {
+    if (argc < 2)
+    {
+        cerr << "Usage: " << argv[0] << "<input files>... <output file>" << endl;
+        return 1;
+    }
+
+    vector<string> inputs(argv + 1, argv + argc - 1);
+    string output(argv[argc - 1]);
+
     auto kernel = createKernel();
-    kernel->startup(std::vector<std::string>{"sample-input"});
+    kernel->startup(inputs);
     kernel->run();
-    kernel->shutdown("sample-output");
+    kernel->shutdown(output);
 }
