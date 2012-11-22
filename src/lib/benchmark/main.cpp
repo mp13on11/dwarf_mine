cedcb68a8dc4b5d2bf96ee1c7e5d8dad4192e468
mp13on11 <>
2012-11-22 17:59:28 +0100

    gold implementation of matrix multiplication

--- src/lib/benchmark/main.cpp
+++ src/lib/benchmark/main.cpp
@@ -7,7 +7,7 @@ BenchmarkKernel::~BenchmarkKernel()
 int main(int argc, const char *argv[])
 {
     auto kernel = createKernel();
-    kernel->startup(std::vector<std::string>{"sample-input"});
+    kernel->startup(std::vector<std::string>{"../a.txt", "../b.txt"});
     kernel->run();
-    kernel->shutdown("sample-output");
+    kernel->shutdown("../c.txt");
 }
