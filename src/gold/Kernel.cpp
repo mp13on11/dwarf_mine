b45bed28ee3fe9902672017a1b296e64302ae08b
mp13on11 <>
2012-11-22 17:03:39 +0100

    changed tabs into 4 spaces

--- src/gold/Kernel.cpp
+++ src/gold/Kernel.cpp
@@ -1,4 +1,5 @@
 #include "Kernel.h"
+#include "../MatrixHelper.h"
 
 #include <iostream>
 #include <memory>
@@ -7,20 +8,22 @@
 
 void Kernel::startup(const std::vector<std::string>& arguments)
 {
-	for(const auto& arg : arguments)
-	std::cout << arg << std::endl;
-}
+    for(const auto& arg : arguments)
+    std::cout << arg << std::endl;
 
+    auto m = MatrixHelper::readMatrixFrom(arguments[0]);
+}
+    
 void Kernel::run()
 {
 }
 
 void Kernel::shutdown(const std::string& outputFilename)
 {
-	std::cout << outputFilename << std::endl;
+    std::cout << outputFilename << std::endl;
 }
 
 std::shared_ptr<BenchmarkKernel> createKernel()
 {
-	return std::shared_ptr<BenchmarkKernel>(new Kernel());
+    return std::shared_ptr<BenchmarkKernel>(new Kernel());
 }
