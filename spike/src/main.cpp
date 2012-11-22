2e9f59c7407d0a4d6b4b93d4e50d733f22eb99e2
mp13on11 <>
2012-11-22 11:53:14 +0100

    add matrix generation
    ./spike generate <filename> generates a random 1000x1000 matrix with values between 0 and 1000

--- spike/src/main.cpp
+++ spike/src/main.cpp
@@ -3,11 +3,15 @@
 #include <iomanip>
 #include <iostream>
 #include <iterator>
+#include <random>
 #include <sstream>
 #include <vector>
 #include "Matrix.h"
 
 using namespace std;
+
+void generateMatrix(const string &fileName);
+void printMatrix(const string &fileName);
 
 template<typename T>
 Matrix<T> readMatrix(const string &fileName);
@@ -18,19 +22,63 @@ void fillMatrixFromStream(Matrix<T> &matrix, istream &stream);
 template<typename T>
 vector<T> getValuesIn(const string &line);
 
+void printUsage(const string &program);
+
 int main(int argc, const char *argv[])
 {
-	if (argc < 2)
+	if (argc < 3)
 	{
-		cerr << "Usage: " << argv[0] << " <matrix file name>" << endl;
+		printUsage(argv[0]);
 		return 1;
 	}
 
 	vector<string> arguments(argv + 1, argv + argc);
 
+	if (arguments[0] == "generate")
+	{
+		generateMatrix(arguments[1]);
+	}
+	else if (arguments[0] == "read")
+	{
+		printMatrix(arguments[1]);
+	}
+	else
+	{
+		printUsage(argv[0]);
+		return 1;
+	}
+}
+
+void generateMatrix(const string &fileName)
+{
+	const size_t size = 1000;
+	default_random_engine engine;
+	uniform_real_distribution<float> distribution(0, 1000);
+
+	ofstream file;
+	file.exceptions(ios_base::failbit);
+	file.open(fileName);
+
+	file << size << " " << size << endl;
+
+	for (size_t i=0; i<size; i++)
+	{
+		if (i % 100 == 0)
+			cout << "written " << i << " rows" << endl;
+		for (size_t j=0; j<size; j++)
+		{
+			file << " " << distribution(engine);
+		}
+
+		file << endl;
+	}
+}
+
+void printMatrix(const string &fileName)
+{
 	try
 	{
-		Matrix<float> matrix = readMatrix<float>(arguments[0]);
+		Matrix<float> matrix = readMatrix<float>(fileName);
 
 		for (size_t i=0; i<matrix.rows(); i++)
 		{
@@ -45,17 +93,14 @@ int main(int argc, const char *argv[])
 	catch (exception &e)
 	{
 		cerr << "Fooooooo:" << e.what() << endl;
-		return 1;
 	}
-
-	Matrix<int> matrix(5, 5);
 }
 
 template<typename T>
 Matrix<T> readMatrix(const string &fileName)
 {
 	ifstream file;
-	file.exceptions(fstream::failbit);
+	file.exceptions(ios_base::failbit);
 	file.open(fileName);
 
 	size_t rows, columns;
@@ -104,4 +149,9 @@ vector<T> getValuesIn(const string &line)
 		);
 
 	return result;
+}
+
+void printUsage(const string &program)
+{
+	cerr << "Usage: " << program << " <generate|read> <matrix file name>" << endl;
 }
