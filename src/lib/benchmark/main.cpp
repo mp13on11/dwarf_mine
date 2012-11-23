93ff38f218da6c81e425aff431d67fac842750b8
mp13on11 <>
2012-11-23 23:20:02 +0100

    now measuring time of Kernel::run() in µs and printing results onto std::out

--- src/lib/benchmark/main.cpp
+++ src/lib/benchmark/main.cpp
@@ -1,11 +1,31 @@
 #include "benchmark/BenchmarkKernel.h"
 
 #include <iostream>
+#include <chrono>
+#include <functional>
+#include <map>
 
 using namespace std;
 
 BenchmarkKernel::~BenchmarkKernel()
 {
+}
+
+map<string, float> benchmark(function<void()> func, size_t iterations = 1)
+{
+    typedef chrono::high_resolution_clock clock;
+    typedef chrono::microseconds microseconds;
+    clock::time_point before = clock::now();
+
+    for(size_t i=0; i<iterations; i++)
+    {
+        func();
+    }
+
+    clock::time_point after = clock::now();
+    microseconds total_ms = (after - before);
+
+    return map<string,float>{{"time (µs)", total_ms.count()}};
 }
 
 int main(int argc, const char* argv[])
@@ -16,18 +36,28 @@ int main(int argc, const char* argv[])
         return 1;
     }
 
+    // split args into first as inputs and the last as output
     vector<string> inputs(argv + 1, argv + argc - 1);
     string output(argv[argc - 1]);
 
     auto kernel = createKernel();
 
+    // check for required number of input arguments
     if (inputs.size() < kernel->requiredInputs())
     {
         cerr << argv[0] << " requires " << kernel->requiredInputs() << " input files..." << endl;
         return 1;
     }
 
+    // execute kernel
     kernel->startup(inputs);
-    kernel->run();
+
+    auto stats = benchmark([&](){kernel->run();});
+
     kernel->shutdown(output);
+
+    // print statistics
+    for (auto& kv : stats) {
+        cout << kv.first << ": " << kv.second << std::endl;
+    }
 }
