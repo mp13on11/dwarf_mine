9464598099f8d90033c5f76bcdec1934dd6379a4
mp13on11 <>
2012-11-25 11:32:34 +0100

    Naive parallel implementation - fails currently with duble free

--- src/mpi/MpiMatrixKernel.cpp
+++ src/mpi/MpiMatrixKernel.cpp
@@ -4,11 +4,16 @@
 
 #include <mpi.h>
 #include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <exception>
+#include <cmath>
 
 using namespace std;
 
 const int MpiMatrixKernel::ROOT_RANK = 0;
-const int MpiMatrixKernel::BLOCK_SIZE = 10;
+const size_t MpiMatrixKernel::BLOCK_ROWS = 3;
+const size_t MpiMatrixKernel::BLOCK_COLUMNS = 3;
 
 MpiMatrixKernel::MpiMatrixKernel() :
         rank(MPI::COMM_WORLD.Get_rank()),
@@ -40,7 +45,7 @@ void MpiMatrixKernel::run()
 
 void MpiMatrixKernel::shutdown(const string& outputFileName)
 {
-    if (rank != ROOT_RANK)
+    if (!isRoot())
         return;
 
     MatrixHelper::writeMatrixTo(outputFileName, result);
@@ -48,12 +53,13 @@ void MpiMatrixKernel::shutdown(const string& outputFileName)
 
 void MpiMatrixKernel::broadcastSizes()
 {
-    size_t sizes[4] = {
-            left.rows(), left.columns(), right.rows(), right.columns()
-        };
+	size_t sizes[4] = {
+			BLOCK_ROWS, left.columns(),
+			right.rows(), BLOCK_COLUMNS
+	};
     MPI::COMM_WORLD.Bcast(sizes, 4 * sizeof(size_t), MPI::CHAR, ROOT_RANK);
 
-    if (rank != ROOT_RANK)
+    if (!isRoot())
     {
         left = Matrix<float>(sizes[0], sizes[1]);
         right = Matrix<float>(sizes[2], sizes[3]);
@@ -61,29 +67,105 @@ void MpiMatrixKernel::broadcastSizes()
     }
 }
 
+vector<float> MpiMatrixKernel::distributeBuffer(const float* sendBuffer, const size_t rows, const size_t columns)
+{
+	size_t size = rows * columns;
+	float* recieveBufferTemp = new float[size];
+	if (MPI_Scatter(
+			const_cast<float*>(sendBuffer), size, MPI_FLOAT,
+			recieveBufferTemp, size, MPI_FLOAT,
+			ROOT_RANK,
+			MPI::COMM_WORLD
+			) < 0)
+	{
+		throw new exception();
+	}
+	vector<float> recieveBuffer;
+	recieveBuffer.assign(recieveBufferTemp, recieveBufferTemp + size);
+	delete[] recieveBufferTemp;
+	return recieveBuffer;
+}
+
+vector<float> MpiMatrixKernel::transposeMatrix(const float* data, const size_t rows, const size_t columns)
+{
+	vector<float> buffer;
+	for (size_t column = 0; column < columns; ++column)
+	{
+		for (size_t row = 0; row < rows; ++row)
+		{
+			buffer.push_back(data[columns*row + column]);
+		}
+	}
+	return buffer;
+}
+
 void MpiMatrixKernel::scatterMatrices()
 {
+    vector<float> leftBuffer = distributeBuffer(left.buffer(), BLOCK_ROWS, left.columns());
+
+    vector<float> rightSendingBuffer;
+    if (isRoot())
+	{
+    	rightSendingBuffer = transposeMatrix(right.buffer(), right.rows(), right.columns());
+	}
+    vector<float> rightRecievingBuffer = distributeBuffer(rightSendingBuffer.data(), BLOCK_COLUMNS, right.rows());
+    vector<float> rightBuffer = transposeMatrix(rightRecievingBuffer.data(), rightRecievingBuffer.size() / BLOCK_COLUMNS, BLOCK_COLUMNS);
+
+    left = Matrix<float>(BLOCK_ROWS, leftBuffer.size() / BLOCK_ROWS, move(leftBuffer));
+	right = Matrix<float>(BLOCK_COLUMNS, rightBuffer.size() / BLOCK_COLUMNS, move(rightBuffer));
 }
 
 void MpiMatrixKernel::multiply()
 {
-    if (rank != ROOT_RANK)
-        return;
-
+	temp = Matrix<float>(left.rows(), right.columns());
     for (size_t i=0; i<left.rows(); i++)
     {
         for (size_t j=0; j<right.columns(); j++)
         {
-            result(i, j) = 0;
-
+        	temp(i, j) = 0;
             for (size_t k=0; k<left.columns(); k++)
             {
-                result(i, j) += left(i, k) * right(k, j);
+            	temp(i, j) += left(i, k) * right(k, j);
             }
         }
     }
 }
 
+void gather(float* sending, Matrix<float>& s, float* recieving, Matrix<float>& r)
+{
+	MPI_Gather(
+			sending, s.rows()* s.columns(), MPI_FLOAT,
+			recieving, r.rows() * r.columns(), MPI_FLOAT,
+			0,
+			MPI::COMM_WORLD
+		);
+}
+
 void MpiMatrixKernel::gatherResult()
 {
+	float* recievingBuffer;
+	if (isRoot())
+	{
+		recievingBuffer = new float[result.rows() * result.columns()];
+	}
+	gather(const_cast<float*>(temp.buffer()), temp, recievingBuffer, result);
+
+	if (isRoot())
+		delete[] recievingBuffer;
+//	if (isRoot())
+//	{
+//		cout << "RANK "<<rank<< " before convert"<<endl;
+////		vector<float> recieveBuffer;
+////		recieveBuffer.assign(recievingBuffer, recievingBuffer + result.rows() * result.columns());
+////		for (size_t i = 0; i< result.rows() * result.columns(); i++)
+////		{
+////			cout<<" "<<i<< "  "<<recievingBuffer[i]<<endl;
+////		}
+////		cout <<endl;
+//		cout << "RANK "<<rank<< " before delete[]"<<endl;
+//		//delete recievingBuffer	;
+//		cout << "RANK "<<rank<< " after delete[]"<<endl;
+//
+//	}
+
 }
--- src/mpi/MpiMatrixKernel.h
+++ src/mpi/MpiMatrixKernel.h
@@ -14,19 +14,29 @@ public:
 
 private:
     static const int ROOT_RANK;
-    static const int BLOCK_SIZE;
+    static const std::size_t BLOCK_ROWS;
+    static const std::size_t BLOCK_COLUMNS;
 
     const int rank;
     const int groupSize;
     Matrix<float> left;
     Matrix<float> right;
     Matrix<float> result;
+    Matrix<float> temp;
 
     void broadcastSizes();
     void scatterMatrices();
     void multiply();
     void gatherResult();
+    std::vector<float> distributeBuffer(const float*, const std::size_t rows, const std::size_t columns);
+    std::vector<float> transposeMatrix(const float*, const std::size_t rows, const std::size_t columns);
+    bool const isRoot() const;
 };
+
+inline bool const MpiMatrixKernel::isRoot() const
+{
+    return rank == ROOT_RANK;
+}
 
 inline std::size_t MpiMatrixKernel::requiredInputs() const
 {
