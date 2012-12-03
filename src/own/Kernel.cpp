2a69d71b46fb0106324da419a1a3f58106949fe0
mp13on11 <>
2012-12-03 17:59:29 +0100

    blocked and parallelized (with openmp) own matrix multiplication

--- src/own/Kernel.cpp
+++ src/own/Kernel.cpp
@@ -5,6 +5,12 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <thread>
+#include <functional>
+#include <math.h>
+#include <omp.h>
+
+using namespace std;
 
 void printMatrix(const Matrix<float>& matrix) 
 {
@@ -35,10 +41,111 @@ void Kernel::startup(const std::vector<std::string>& arguments)
     c = Matrix<float>(a.rows(), b.columns());
 }
     
+size_t div_ceil(size_t x, size_t y)
+{
+    return (x % y) ? (x / y + 1) : (x / y);
+}
+
+void multiplyBlock(Matrix<float>& left, Matrix<float>& right, Matrix<float>& outp, size_t workerCount, size_t index) {
+        Matrix<float>& out = outp;
+        //cout << "Launched by thread " << index << endl;
+
+        size_t totalElements = out.rows() * out.columns();
+        size_t elementsPerBlock = div_ceil(totalElements, workerCount);
+        cout << "elementsPerBlock: " << elementsPerBlock << endl;
+
+        size_t columnsPerBlock = (size_t)ceil(sqrt(elementsPerBlock));
+        columnsPerBlock = min(columnsPerBlock, out.columns());
+
+        size_t rowsPerBlock = div_ceil(elementsPerBlock, columnsPerBlock);
+        cout << "rowsPerBlock: " << rowsPerBlock << endl;
+        cout << "columnsPerBlock: " << columnsPerBlock << endl;
+
+        size_t blocksPerRow = div_ceil(out.columns(), columnsPerBlock);
+        size_t blocksPerColumns = div_ceil(out.rows(), rowsPerBlock);
+
+        size_t rowStart = (index / blocksPerRow) * rowsPerBlock;
+        size_t columnStart = (index % blocksPerRow) * columnsPerBlock;
+
+        cout << "beginngin from (" << columnStart << "," << rowStart << ")" << endl;
+
+        size_t columnEnd = min(columnStart+columnsPerBlock, out.columns());
+        size_t rowEnd = min(rowStart+rowsPerBlock, out.rows());
+        for(size_t y=columnStart; y<columnEnd; y++)
+        {        
+            for(size_t x=rowStart; x<rowEnd; x++)
+            {
+                float val = 0;
+                for(size_t i=0; i<left.columns(); i++)
+                {
+                    val += left(y,i) * right(i,x);
+                }
+                //cout << "calculating out(" << y << "," << x << ") = " << val << endl;
+                out(y,x) = val;
+            }
+        }
+    }
+
 void Kernel::run()
 {
+    size_t workerCount = 5;
+    thread* workers = new thread[workerCount];
 
-    for(size_t y=0; y<c.rows(); y++)
+    //function<void(size_t)> workerMethod = bind(multiplyBlock, a, b, c, workerCount);
+
+    //omp_set_num_threads(8);
+
+    size_t elementsPerBlock = 10*10;
+
+    size_t columnsPerBlock = (size_t)ceil(sqrt(elementsPerBlock));
+    columnsPerBlock = min(columnsPerBlock, c.columns());
+
+    size_t rowsPerBlock = div_ceil(elementsPerBlock, columnsPerBlock);
+
+    size_t blocksPerRow = div_ceil(c.columns(), columnsPerBlock);
+    size_t blocksPerColumns = div_ceil(c.rows(), rowsPerBlock);
+
+
+
+
+
+    size_t blockCount = blocksPerRow * blocksPerColumns;
+    //cout << "blockCount: " << blockCount << endl;
+
+    #pragma omp parallel for
+    for(size_t index=0; index<blockCount; index++)
+    {
+        size_t rowStart = (index / blocksPerRow) * rowsPerBlock;
+        size_t columnStart = (index % blocksPerRow) * columnsPerBlock;
+
+        size_t columnEnd = min(columnStart+columnsPerBlock, c.columns());
+        size_t rowEnd = min(rowStart+rowsPerBlock, c.rows());
+
+        //cout << rowStart << ":" << columnStart << " - " << rowEnd << ":" << columnEnd << endl;
+
+        for(size_t y=rowStart; y<rowEnd; y++)
+        {        
+            for(size_t x=columnStart; x<columnEnd; x++)
+            {
+                float val = 0;
+                for(size_t i=0; i<a.columns(); i++)
+                {
+                    val += a(y,i) * b(i,x);
+                }
+                //cout << "calculating c(" << y << "," << x << ") = " << val << endl;
+                c(y,x) = val;
+            }
+        }
+
+    }
+//        workers[i] = thread(multiplyBlock, a, b, c, workerCount, i);
+
+    //for(size_t i=0; i<workerCount; i++)
+    //    workers[i].join();
+
+    delete[] workers;
+
+    /*for(size_t y=0; y<c.rows(); y++)
     {        
         for(size_t x=0; x<c.columns(); x++)
         {
@@ -50,7 +157,7 @@ void Kernel::run()
             }
             c(y,x) = val;
         }
-    }
+    }*/
 }
 
 void Kernel::shutdown(const std::string& outputFilename)
