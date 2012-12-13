4dbdbd466bf46fb7fe3c19cee1ff075a730af312
mp13on11 <>
2012-12-13 18:41:17 +0100

    add virtual destructor to Scheduler

--- src/main/Scheduler.h
+++ src/main/Scheduler.h
@@ -8,5 +8,10 @@ class Scheduler
 {
 public:
     Scheduler(const BenchmarkResult& benchmarkResult);
+    virtual ~Scheduler();
     virtual void dispatch(ProblemStatement& statement);
 };
+
+inline Scheduler::~Scheduler()
+{
+}
