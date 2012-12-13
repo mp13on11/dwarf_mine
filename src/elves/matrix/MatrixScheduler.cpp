b1895ead7a1da7cd7ffca0ab6a7722c3d625969e
mp13on11 <>
2012-12-13 16:26:20 +0100

    Stub for MatrixScheduler

---
+++ src/elves/matrix/MatrixScheduler.cpp
@@ -0,0 +1,11 @@
+#include "MatrixScheduler.h"
+
+MatrixScheduler::MatrixScheduler(const BenchmarkResult& benchmarkResult)
+: Scheduler(benchmarkResult)
+{
+}
+
+void MatrixScheduler::dispatch(ProblemStatement& statement)
+{
+    
+}
