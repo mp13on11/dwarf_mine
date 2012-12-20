1cea5265db807ea5cd98f5737377162fd6c2d87c
mp13on11 <>
2012-12-20 17:50:33 +0100

    BenchmarkRunner now returns relative results

--- src/elves/matrix/MatrixScheduler.cpp
+++ src/elves/matrix/MatrixScheduler.cpp
@@ -5,10 +5,27 @@
 #include <iostream>
 #include <memory>
 #include <mpi.h>
+#include <numeric>
+#include "Matrix.h"
+#include <cmath>
+#include <list>
+#include <map>
 
 using namespace std;
 
 const int MASTER = 0; // TODO: Put in one common .h file
+
+struct MatrixSlice
+{
+    size_t x;
+    size_t y;
+    size_t columns;
+    size_t rows;
+};
+
+void spliceColumns(vector<MatrixSlice>& slices, list<int>& relation, int rowOrigin, int columnOrigin, int rows, int columns);
+void spliceRows(vector<MatrixSlice>& slices, list<int>& relation, int rowOrigin, int columnOrigin, int rows, int columns);
+
 
 MatrixScheduler::MatrixScheduler(const BenchmarkResult& benchmarkResult) :
     Scheduler(benchmarkResult)
@@ -33,8 +50,82 @@ void send(istream& stream, int toRank)
     MPI::COMM_WORLD.Send(buffered.c_str(), buffered.size(), MPI::CHAR, toRank, 0);
 }
 
+void spliceColumns(vector<MatrixSlice>& slices, list<int>& relation, int rowOrigin, int columnOrigin, int rows, int columns)
+{
+    int processor = relation.size() - 1;
+    if (processor < 0)
+    {
+        return;
+    }
+
+    int pivot = columns;
+    if (relation.size() > 1)
+    {
+        int overall = 0;
+        for (const auto& s : relation)
+        {
+            overall += s;
+        }
+        pivot = ceil(columns * relation.front() * 1.0 / overall);
+    }
+    slices[processor].x = columnOrigin;
+    slices[processor].y = rowOrigin;
+    slices[processor].columns = columnOrigin;
+    slices[processor].rows = rows + pivot;
+    relation.pop_front();
+    spliceRows(slices, relation, rowOrigin, columnOrigin + pivot, rows, columns - pivot);
+}
+
+void spliceRows(vector<MatrixSlice>& slices, list<int>& relation, int rowOrigin, int columnOrigin, int rows, int columns)
+{
+    int processor = relation.size() - 1;
+    if (processor < 0)
+    {
+        cout << relation.size() << endl;
+        return;
+    }
+
+    int pivot = rows;
+    if (relation.size() > 1)
+    {
+        int overall = 0;
+        for (const auto& s : relation)
+        {
+            overall += s;
+        }
+        pivot = ceil(rows * relation.front() * 1.0 / overall);
+    }
+    slices[processor].x = columnOrigin;
+    slices[processor].y = rowOrigin;
+    slices[processor].columns = columnOrigin + pivot;
+    slices[processor].rows = rows;
+    relation.pop_front();
+    spliceColumns(slices, relation, rowOrigin + pivot, columnOrigin, rows - pivot, columns);
+}
+
+vector<MatrixSlice> spliceAndDice(Matrix<float>& map, list<int>& relation)
+{
+    vector<MatrixSlice> slices(relation.size());
+    spliceColumns(slices, relation, 0, 0, map.rows(), map.columns());
+    return slices;
+}
+
+// vector<char> packMatrices(const vector<Matrix<float>>& matrices)
+// {
+
+// }
+
+// vector<Matrix<float>> unpackMatrices(const vector<char> buffer)
+// {
+
+// }
+
+
 void MatrixScheduler::doDispatch(ProblemStatement& statement)
 {
+    // Matrix<float> _map(93,37);
+    // list<int> relation ={5, 3, 3, 3, 1};
+    // spliceAndDice(_map, relation);
     if (rank == MASTER)
     {
         if (nodeSet.size() == 1)
--- src/main/BenchmarkRunner.cpp
+++ src/main/BenchmarkRunner.cpp
@@ -2,6 +2,7 @@
 #include <mpi.h>
 #include <vector>
 #include <memory>
+#include <cmath>
 #include "Elf.h"
 #include "BenchmarkRunner.h"
 #include "Scheduler.h"
@@ -25,7 +26,7 @@ std::chrono::microseconds BenchmarkRunner::measureCall(ProblemStatement& stateme
     return clock::now() - before;
 }
 
-void BenchmarkRunner::benchmarkDevice(DeviceId device, ProblemStatement& statement, Scheduler& scheduler)
+void BenchmarkRunner::benchmarkDevice(DeviceId device, ProblemStatement& statement, Scheduler& scheduler, BenchmarkResult& results)
 {
     for (size_t i = 0; i < WARMUP_ITERATIONS; ++i)
     {
@@ -36,7 +37,7 @@ void BenchmarkRunner::benchmarkDevice(DeviceId device, ProblemStatement& stateme
     {
         sum += measureCall(statement, scheduler);
     }
-    m_results[device] = (sum / _iterations).count();
+    results[device] = (sum / _iterations).count();
 }
 
 void BenchmarkRunner::getBenchmarked(ProblemStatement& statement, Scheduler& scheduler)
@@ -54,11 +55,13 @@ void BenchmarkRunner::runBenchmark(ProblemStatement& statement, const ElfFactory
 
     if (MPI::COMM_WORLD.Get_rank() == MASTER)
     {
+        BenchmarkResult measurements;
         for (NodeId device = 0; device < _devices; ++device)
         {
             scheduler->setNodeset(device);
-            benchmarkDevice(device, statement, *scheduler);
+            benchmarkDevice(device, statement, *scheduler, measurements);
         }
+        transformToResults(measurements);
     }
     else
     {
@@ -66,8 +69,20 @@ void BenchmarkRunner::runBenchmark(ProblemStatement& statement, const ElfFactory
     }
 }
 
+void BenchmarkRunner::transformToResults(const BenchmarkResult& result)
+{
+    int runtimeSum = 0;
+    for (const auto& nodeResult : result)
+    {
+        runtimeSum += nodeResult.second;
+    }
+    for (const auto& nodeResult : result)
+    {
+        _results[nodeResult.first] = 100 - nodeResult.second * 100 / runtimeSum;
+    }
+}
 
 BenchmarkResult BenchmarkRunner::getResults()
 {
-    return m_results;
+    return _results;
 }
