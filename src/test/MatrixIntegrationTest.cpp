fd1376bdbbf8479ab1904f7dd30a9386682e0313
mp13on11 <>
2013-01-10 17:12:40 +0100

    integration tests waits with timeout for the mpirun subprocess

--- src/test/MatrixIntegrationTest.cpp
+++ src/test/MatrixIntegrationTest.cpp
@@ -8,13 +8,51 @@
 #include <cstdlib>
 #include <string>
 #include <fstream>
+#include <future>
+#include <sys/wait.h>
+#include <signal.h>
 
 using namespace std;
 
 TEST(MatrixIntegrationTest, TestItNowwwwww)
 {
-    system("mpirun -n 8 --tag-output build/src/main/dwarf_mine -m smp -n 1 -w 0 -i" 
-        "small_input.bin -o small_output.bin --import_configuration test_config.cfg");
+    const int TIMEOUT_SECONDS = 10;
+    pid_t pid = fork();
+    if(pid == 0) // child process
+    {
+        char* argv[] = {(const char*)"/usr/bin/mpirun", 
+                        (const char*)"-n", "8",
+                        (const char*)"--tag-output",
+                        (const char*)"build/src/main/dwarf_mine", 
+                        (const char*)"-m", "smp", 
+                        (const char*)"-n", "1",
+                        (const char*)"-w", "0",
+                        (const char*)"-q",
+                        (const char*)"-i", "small_input.bin",
+                        (const char*)"-o", "small_output.bin",
+                        (const char*)"--import_configuration", "test_config.cfg",
+                        nullptr};
+        execv(argv[0], argv);
+        exit(-1);
+    }
+
+    auto future = async(std::launch::async, [pid]()->bool
+    {
+        int status;
+        waitpid(pid, &status, 0);
+        return WIFEXITED(status) && (WEXITSTATUS(status) == 0);
+    });
+
+    auto status = future.wait_for(std::chrono::seconds(TIMEOUT_SECONDS));
+
+    if(status != future_status::ready)
+    {
+        kill(pid, SIGKILL);
+        future.wait();
+        FAIL() << "Process timed out";
+    }
+
+    ASSERT_TRUE(future.get()) << "Process not exited normally";
 
     ifstream input("small_input.bin", ios_base::binary);
     auto inputMatrices = MatrixHelper::readMatrixPairFrom(input);
