617f69bd3db3bde8bcfa3526c88b8ffd8f44bae5
mp13on11 <>
2013-01-10 15:08:46 +0100

    extract SequentialFactorizer from FactorizationElf

---
+++ src/elves/factorize/smp/SequentialFactorizer.cpp
@@ -0,0 +1,91 @@
+#include "SequentialFactorizer.h"
+
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+SequentialFactorizer::SequentialFactorizer(const BigInt& number) :
+        m(number), p(0), q(0), distribution(), engine(time(NULL)),
+        generator(bind(distribution, engine))
+{
+}
+
+void SequentialFactorizer::run()
+{
+    while (p == 0 && q == 0)
+    {
+        BigInt a = generateRandomNumberSmallerThan(m);
+        BigInt aSquared = a * a;
+        BigInt remainder = aSquared % m;
+
+        iterator_pair range = remainders.equal_range(remainder);
+
+        for (iterator it = range.first; it != range.second; it++)
+        {
+            const BigInt& b = it->second;
+            BigInt bSquared = b * b;
+
+            if (a > b)
+            {
+                if (aSquared - bSquared == m)
+                {
+                    BigInt tmp = a + b;
+
+                    if (tmp == BigInt::ONE || tmp == m)
+                        continue;
+
+                    p = tmp;
+                    q = a - b;
+
+                    return;
+                }
+            }
+            else
+            {
+                if (bSquared - aSquared == m)
+                {
+                    BigInt tmp = a + b;
+
+                    if (tmp == BigInt::ONE || tmp == m)
+                        continue;
+
+                    p = tmp;
+                    q = b - a;
+
+                    return;
+                }
+            }
+        }
+
+        remainders.insert(pair<BigInt, BigInt>(remainder, a));
+    }
+}
+
+BigInt SequentialFactorizer::generateRandomNumberSmallerThan(const BigInt& number) const
+{
+    BigInt result;
+    size_t maximumItems = number.buffer().size();
+
+    if (maximumItems == 1)
+    {
+        uint32_t n = number.buffer().back();
+        return BigInt(generator() % n);
+    }
+
+    do
+    {
+        size_t actualItems = generator() % maximumItems + 1;
+        vector<uint32_t> items;
+
+        for (size_t i=0; i<actualItems; i++)
+        {
+            items.push_back(generator());
+        }
+
+        result = BigInt(items);
+    }
+    while (result >= number);
+
+    return result;
+}
--- src/elves/factorize/smp/SmpFactorizationElf.h
+++ src/elves/factorize/smp/SmpFactorizationElf.h
@@ -2,25 +2,8 @@
 
 #include "elves/factorize/FactorizationElf.h"
 
-#include <functional>
-#include <map>
-#include <random>
-
 class SmpFactorizationElf : public FactorizationElf
 {
 public:
-    SmpFactorizationElf();
-
     virtual std::pair<BigInt, BigInt> factorize(const BigInt& number);
-
-private:
-    std::multimap<BigInt, BigInt> remainders;
-    std::uniform_int_distribution<uint32_t> distribution;
-    std::mt19937 engine;
-    std::function<uint32_t()> generator;
-
-    typedef std::multimap<BigInt, BigInt>::iterator iterator;
-    typedef std::pair<iterator, iterator> iterator_pair;
-
-    BigInt generateRandomNumberSmallerThan(const BigInt& n) const;
 };
