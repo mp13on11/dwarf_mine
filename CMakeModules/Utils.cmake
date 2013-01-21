8cbcc7e031925c6f29a516b1070f1ddac339a9cd
mp13on11 <>
2013-01-21 23:49:20 +0100

    Moved C++ 11 compiler flag up to top-level CMakeLists.txt, some cleanup in CMake files

--- CMakeModules/Utils.cmake
+++ CMakeModules/Utils.cmake
@@ -9,11 +9,3 @@ MACRO(ADD_COMMON_MSVC_PROPS EXECUTABLE_NAME)
             LINK_FLAGS "/NODEFAULTLIB:libcmt /SUBSYSTEM:CONSOLE")
     ENDIF()
 ENDMACRO()
-
-MACRO(ENABLE_CPP_11)
-    IF(CMAKE_COMPILER_IS_GNUCXX)
-        # Enable C++ 11 and threading for gcc
-        ADD_CXX_FLAGS("-std=c++0x -pthread")
-    ENDIF()
-ENDMACRO()
-
