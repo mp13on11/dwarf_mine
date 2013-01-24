dce97901092f4da2859c86920fbb5deba8a7c1e9
mp13on11 <>
2013-01-24 13:58:53 +0100

    reorder Methods in Benchmark-Header

--- src/common/BenchmarkRunner.h
+++ src/common/BenchmarkRunner.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <vector>
 #include <chrono>
 #include <memory>
+#include <vector>
 #include "BenchmarkResults.h"
 #include "Configuration.h"
 #include "ProblemStatement.h"
@@ -12,6 +12,13 @@ class Scheduler;
 
 class BenchmarkRunner
 {
+public:
+    explicit BenchmarkRunner(const Configuration& config);
+    BenchmarkRunner(const Configuration& config, const BenchmarkResult& result);
+    void runBenchmark(ProblemStatement& statement, const SchedulerFactory& factory);
+    BenchmarkResult getWeightedResults();
+    BenchmarkResult getTimedResults();
+
 private:
     size_t _iterations;
     size_t _warmUps;
@@ -23,11 +30,4 @@ private:
     unsigned int benchmarkNodeset(ProblemStatement& statement, Scheduler& scheduler);
     void getBenchmarked(Scheduler& scheduler);
     void weightTimedResults();
-
-public:
-    explicit BenchmarkRunner(const Configuration& config);
-    BenchmarkRunner(const Configuration& config, const BenchmarkResult& result);
-    void runBenchmark(ProblemStatement& statement, const SchedulerFactory& factory);
-    BenchmarkResult getWeightedResults();
-    BenchmarkResult getTimedResults();
 };
