0f21713ca6b9cdbcab16fc69d385daff2b9057e9
mp13on11 <>
2013-02-22 13:04:54 +0100

    Blubb

--- src/common/BenchmarkResults.cpp
+++ src/common/BenchmarkResults.cpp
@@ -1,5 +1,7 @@
 #include "BenchmarkResults.h"
 #include <iostream>
+#include <boost/range/algorithm/copy.hpp>
+#include <boost/range/adaptor/map.hpp>
 
 std::ostream& operator<<(std::ostream& o, BenchmarkResult& results)
 {
