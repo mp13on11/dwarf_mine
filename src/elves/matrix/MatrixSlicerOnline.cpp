7bff651a10ceee0d52474bfacaeadec8931da857
mp13on11 <>
2013-03-09 10:43:11 +0100

    Fixed MatrixOnlineSlicer layouting

--- src/elves/matrix/MatrixSlicerOnline.cpp
+++ src/elves/matrix/MatrixSlicerOnline.cpp
@@ -9,10 +9,15 @@ vector<MatrixSlice> MatrixSlicerOnline::layout(size_t rows, size_t columns, size
 {
     slices.clear();
     size_t slice_rows = ceil((double)rows / rowparts);
-    size_t slice_cols = ((double)columns / colparts);
-    for(size_t i=0; i<=columns; i+= slice_cols){
-        for(size_t j=0; j<=rows; j+= slice_rows){
-            slices.push_back(MatrixSlice{i, j, slice_cols, slice_rows});
+    size_t slice_cols = ceil((double)columns / colparts);
+    for(size_t i=0; i<columns; i+= slice_cols){
+        for(size_t j=0; j<rows; j+= slice_rows){
+            slices.push_back(MatrixSlice{
+                i,
+                j,
+                i + slice_cols <= columns ? slice_cols : slice_cols - (i + slice_cols - columns),
+                j + slice_rows <= rows ? slice_rows : slice_rows - (j + slice_rows - rows)
+            });
         }
     }
     return slices;
