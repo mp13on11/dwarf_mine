9ec9a1e5db35f644ce4ccf204c1ff3731dc59b82
mp13on11 <>
2013-03-14 10:52:53 +0100

    Refactored randomNumber generation for CUDA into stubbed an non-stubbed version to reduce branch diversion (dropped metric by 4%)

--- src/elves/montecarlo/cuda/CudaUtil.cuh
+++ src/elves/montecarlo/cuda/CudaUtil.cuh
@@ -33,25 +33,27 @@ const int FIELD_DIMENSION = 8;
 		assert(CONDITION); \
 	}
 
-
+/***
+ * Delivers a "random" number
+ */
+__device__ size_t randomNumber(curandState* deviceStates, size_t maximum, float fakedRandom)
+{
+	size_t result = size_t(floor(fakedRandom * maximum));
+	//cassert(result < maximum, "Random %f - Maximum %lu = %f = %lu\n", random, maximum, random * maximum, result);
+    return result;
+}
 
 /***
  * Delivers a random number x with 0 <= x < maximum
  */
-__device__ size_t randomNumber(curandState* deviceStates, size_t maximum, float fakedRandom = -1)
+__device__ size_t randomNumber(curandState* deviceStates, size_t maximum)
 {
 	size_t threadGeneratorIndex = blockIdx.x;
 	float random = 0;
-	if (fakedRandom >= 0)
-	{
-		random = fakedRandom;
-	}
-	else
-	{
-		curandState deviceState = deviceStates[threadGeneratorIndex];
-		random = 1.0f - curand_uniform(&deviceState); // delivers (0, 1] - we need [0, 1)
-		deviceStates[threadGeneratorIndex] = deviceState;
-	}
+	
+	curandState deviceState = deviceStates[threadGeneratorIndex];
+	random = 1.0f - curand_uniform(&deviceState); // delivers (0, 1] - we need [0, 1)
+	deviceStates[threadGeneratorIndex] = deviceState;
 	size_t result = size_t(floor(random * maximum));
 	//cassert(result < maximum, "Random %f - Maximum %lu = %f = %lu\n", random, maximum, random * maximum, result);
     return result;
