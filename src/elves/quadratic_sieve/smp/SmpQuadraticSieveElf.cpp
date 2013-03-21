a88c29b7cf2870024ba0b36c2f837490096fe2b6
mp13on11 <>
2013-03-21 15:47:31 +0100

    Removed debug prints

--- src/elves/quadratic_sieve/smp/SmpQuadraticSieveElf.cpp
+++ src/elves/quadratic_sieve/smp/SmpQuadraticSieveElf.cpp
@@ -41,7 +41,6 @@ vector<BigInt> smpSieveKernel(const BigInt& start, const BigInt& end, const BigI
             for(const BigInt& root : roots)
             {
                 BigInt offset = (primePower + root - (start % primePower)) % primePower;
-                cout << "offset for prime " << offset << " " << primePower << endl;
                 for(BigInt j=offset; j<=blockSize; j+=primePower)
                 {
                     logs[j.get_ui()] -= primeLog;
@@ -58,10 +57,8 @@ vector<BigInt> smpSieveKernel(const BigInt& start, const BigInt& end, const BigI
 
     for(uint32_t i=0; i<=blockSize; i++)
     {
-        //cout << "log: " << logs[i] << endl;
         if(logs[i] < logTreshold) // probable smooth
         {
-            cout << "result: " << start + i;
             result.emplace_back(start+i);
         }
     }
