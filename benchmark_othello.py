dc269582548ca668af0849bd30c57eba699c46b6
mp13on11 <>
2013-03-25 17:45:43 +0100

    plotting läuft mehr oder weniger

--- benchmark_othello.py
+++ benchmark_othello.py
@@ -10,15 +10,18 @@ from pylab import *
 
 import calendar
 
-DIRECTORY_NAME = "values2"
+import random
+import time
+
+DIRECTORY_NAME = "values"
 ITERATIONS = 5
+DEBUG = True
 
 def collectMeasuresForTrial(trial):
 	smp = []
 	cuda = []
 	
 	for fileName in sorted(os.listdir(DIRECTORY_NAME)):
-		print fileName
 		fileNameParts = fileName.split("_")
 		date = fileNameParts[0]
 		mode = fileNameParts[1]
@@ -32,9 +35,23 @@ def collectMeasuresForTrial(trial):
 			
 	return smp, cuda
 
-def collectMeasuresForRevision(directoryName):
+def collectMeasuresForRevision(rev):
 	smp = []
 	cuda = []
+	
+	for fileName in sorted(os.listdir(DIRECTORY_NAME)):
+		fileNameParts = fileName.split("_")
+		date = fileNameParts[0]
+		mode = fileNameParts[1]
+		fileTrial = fileNameParts[2]
+		fileRev = fileNameParts[3][:-4]
+		
+		if fileRev == rev:
+			if mode == "smp":
+				smp.append((date, avgTimeFromFile(fileName)))
+			if mode == "cuda":
+				cuda.append((date, avgTimeFromFile(fileName)))
+	
 	return smp, cuda
 
 def collectData(directoryName):
@@ -51,52 +68,76 @@ def collectData(directoryName):
 	return set(timestamps_hash), set(trials)
 
 def avg(xs):
-	#print "===", xs
 	return int(sum(xs) / len(xs))
 
 def avgTimeFromFile(fileName):
-	#print "==", fileName
 	return avg([float(line) for line in open(os.path.join(DIRECTORY_NAME, fileName))])
 
+def plotTrialsForRevision(smp, cuda, rev, fileName):
+		
+	ind = arange(len(smp))
+	width = 0.35
+	
+	fig = pyplot.figure()
+	ax = fig.add_subplot(111)
+	ax.plot(ind, [value for date, value in smp], label = "SMP", color = "r")
+	ax.plot(ind + width, [value for date, value in cuda], label = "CUDA", color = "y")
+	
+	ax.set_ylabel("Runtime")
+	ax.set_title("Revision:" + rev)
+	ax.set_xticks(ind)
+	ax.set_xticklabels([date for date, value in smp])
+	
+	ax.legend(loc = 2)
+	
+	pyplot.savefig(fileName, bbox_inches = "tight")
+
+	
 def plotChange(smp, cuda, trial, fileName):
 	
-	pyplot.title("Othello Trials: " + trial)
-	pyplot.xlabel('Revision')
-	pyplot.ylabel('Runtime')
+	ind = arange(len(smp))
+	width = 0.35
 	
-	pyplot.xticks(arange(len(smp)), [date for date, value in smp])
-	pyplot.plot([value for date, value in smp])
-	pyplot.plot([value for date, value in cuda])
-	pyplot.legend(["SMP", "CUDA"])
-
-	pyplot.savefig(fileName)
+	fig = pyplot.figure()
+	ax = fig.add_subplot(111)
+	rects1 = ax.bar(ind, [value for date, value in smp], width, label = "SMP", color = "r")
+	rects2 = ax.bar(ind + width, [value for date, value in cuda], width, label = "CUDA", color = "y")
+	
+	ax.set_ylabel("Runtime")
+	ax.set_title("Othello trials:" + trial)
+	ax.set_xticks(ind + width)
+	ax.set_xticklabels([date for date, value in smp])
+	
+	ax.legend(loc = 2)
+	
+	pyplot.savefig(fileName, bbox_inches = "tight")
 
 def getCurrentRev():
 	proc = subprocess.Popen(["git log --pretty=format:'%H_%ct' -n 1"], stdout=subprocess.PIPE, shell=True)
-	return proc.communicate()[0]
+	rev, date = proc.communicate()[0].split("_")
 
-def createFileName(rev, mode, numberOfIterations, directory):
-	return directory + "/" + mode + "_" + str(numberOfIterations) + "_" + str(rev)+ ".txt"
+def createFileName(rev, date, mode, numberOfIterations):
+	return DIRECTORY_NAME + "/" + date + "_" + mode + "_" + str(numberOfIterations) + "_" + str(rev)+ ".txt"
 
-def getCommandFor(directory, numberOfIterations, mode):
-	rev = getCurrentRev()
-	return "./build/src/main/dwarf_mine -m {0} -c montecarlo_tree_search -n 100 -w 10 --montecarlo_trials {1} -q --time_output {2}".format(mode, numberOfIterations, createFileName(rev, mode, numberOfIterations, directory))
+def getCommandFor(numberOfIterations, mode, rev, date):
+	return "./build/src/main/dwarf_mine -m {0} -c montecarlo_tree_search -n 100 -w 10 --montecarlo_trials {1} -q --time_output {2}".format(mode, numberOfIterations, createFileName(rev, date, mode, numberOfIterations))
 
 def meassure(numberOfIterations, mode):
 	
 	if not os.path.exists(DIRECTORY_NAME):
 		os.makedirs(DIRECTORY_NAME)
-	command = getCommandFor(DIRECTORY_NAME, numberOfIterations, mode,)
-	print command
+	rev, date = getCurrentRev()
+	command = getCommandFor(numberOfIterations, mode, rev, date)
 	value = os.system(command)
 
 if __name__ == "__main__":
 	start = 10
 	modes = ["smp", "cuda"]
 
-	for i in range(1, ITERATIONS):
+	for i in range(1, ITERATIONS + 1):
 		for mode in modes:
 			pass
+	#		meassure(10**i, mode)
 
 	timestamp_hashes, trials = collectData(DIRECTORY_NAME)
 
@@ -105,16 +146,5 @@ if __name__ == "__main__":
 		plotChange(smp, cuda, trial, "change_" + trial)
 
 	for timestamp_hash in timestamp_hashes:
-		smp, cuda = collectMeasuresForRevision(timestamp_hash)
-
-	#for r,d,files in os.walk(DIRECTORY_NAME):
-	#	revision_dates = []
-#
-#		for f in files:
-#			date = f.split("_")[1]
-#			if date not in revision_dates:
-#				revision_dates.append(date)
-#			threadTimes = timesFromFile(os.path.join(DIRECTORY_NAME, f))
-#
-
-#	print revision_dates
+		smp, cuda = collectMeasuresForRevision(timestamp_hash[1])
+		plotTrialsForRevision(smp, cuda, timestamp_hash[0], "rev_" + timestamp_hash[1])
