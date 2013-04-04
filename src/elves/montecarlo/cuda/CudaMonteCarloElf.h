029d0f87bfcda513665d3014fba5d7fa92d3e208
mp13on11 <>
2013-04-04 09:50:26 +0200

    Tab to spaces

--- src/elves/montecarlo/cuda/CudaMonteCarloElf.h
+++ src/elves/montecarlo/cuda/CudaMonteCarloElf.h
@@ -9,6 +9,6 @@ class CudaMonteCarloElf : public MonteCarloElf
 public:
     virtual OthelloResult getBestMoveFor(OthelloState& state, size_t reiterations, size_t nodeId, size_t commonSeed);
 private:
-	OthelloResult getBestMoveForSingleStream(OthelloState& state, size_t reiterations, size_t nodeId, size_t commonSeed);
-	OthelloResult getBestMoveForMultipleStream(OthelloState& state, size_t reiterations, size_t nodeId, size_t commonSeed);
+    OthelloResult getBestMoveForSingleStream(OthelloState& state, size_t reiterations, size_t nodeId, size_t commonSeed);
+    OthelloResult getBestMoveForMultipleStream(OthelloState& state, size_t reiterations, size_t nodeId, size_t commonSeed);
 };
