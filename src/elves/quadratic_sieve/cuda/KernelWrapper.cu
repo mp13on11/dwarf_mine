624a3b802fafcb3e4dd838ed13a85f4506d8be7c
lethal-guitar <lethal_guitar128@web.de>
2013-04-18 12:34:30 +0200

    Revert "Added LICENSE header to all source files"
    This reverts commit 55284e77f97741df18bfb31f9f4e79a2e163b43e.

--- src/elves/quadratic_sieve/cuda/KernelWrapper.cu
+++ src/elves/quadratic_sieve/cuda/KernelWrapper.cu
@@ -1,30 +1,3 @@
-/*****************************************************************************
-* Dwarf Mine - The 13-11 Benchmark
-*
-* Copyright (c) 2013 Bünger, Thomas; Kieschnick, Christian; Kusber,
-* Michael; Lohse, Henning; Wuttke, Nikolai; Xylander, Oliver; Yao, Gary;
-* Zimmermann, Florian
-*
-* Permission is hereby granted, free of charge, to any person obtaining
-* a copy of this software and associated documentation files (the
-* "Software"), to deal in the Software without restriction, including
-* without limitation the rights to use, copy, modify, merge, publish,
-* distribute, sublicense, and/or sell copies of the Software, and to
-* permit persons to whom the Software is furnished to do so, subject to
-* the following conditions:
-*
-* The above copyright notice and this permission notice shall be
-* included in all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*****************************************************************************/
-
 #include "kernel.cuh"
 #include "Number.cuh"
 #include "Factorize.h"
@@ -56,3 +29,4 @@ void megaWrapper(const uint32_t* number, uint32_t* logs_d, const uint32_t* facto
 	cudaFree(start_d);
 	cudaFree(end_d);
 	cudaFree(number_d);
+}
